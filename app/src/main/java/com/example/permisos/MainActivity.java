d7a7a767b7366bbfaded79e6a42c93c58bb606fe
marilu <romeromarilu13@gmail.com>
2019-03-24 17:45:33 -0600

    Modificación de los permisos

--- app/src/main/java/com/example/permisos/MainActivity.java
+++ app/src/main/java/com/example/permisos/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.permisos;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -9,67 +10,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-Button btncamera, btngps;
+
+    Switch camaraSwitch, audioSwitch, ubicacionSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btncamera = findViewById(R.id.btncamera);
-        btngps = findViewById(R.id.btngps);
 
+        camaraSwitch = findViewById(R.id.camaraSwitch);
+        audioSwitch = findViewById(R.id.audioSwitch);
+        ubicacionSwitch = findViewById(R.id.ubicacionSwitch);
 
-        View.OnClickListener click = new View.OnClickListener()
-        {
+        permisoCamara();
+        permisoUbicacion();
+        permisoAudio();
+
+        View.OnClickListener click = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch (v.getId())
                 {
-                    case R.id.btncamera:
-                        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-                        {
-                            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.CAMERA))
-                            {
-                                Toast.makeText(MainActivity.this,"Aceptalo", Toast.LENGTH_SHORT).show();
-                                String[] lp = new String[] {Manifest.permission.CAMERA};
-                                ActivityCompat.requestPermissions(MainActivity.this,lp,1);
-                            }
-                            else
-                            {
-                                String[] lp = new String[] {Manifest.permission.CAMERA};
-                                ActivityCompat.requestPermissions(MainActivity.this,lp,1);
-                            }
-                        }
+                    case R.id.camaraSwitch:
+                        permisoCamara();
                         break;
-
-                    case R.id.btngps:
-                        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-                        {
-                            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.ACCESS_FINE_LOCATION))
-                            {
-                                Toast.makeText(MainActivity.this,"Aceptalo", Toast.LENGTH_SHORT).show();
-                                String[] lp = new String[] {Manifest.permission.ACCESS_FINE_LOCATION};
-                                ActivityCompat.requestPermissions(MainActivity.this,lp,2);
-                            }
-                            else
-                            {
-                                String[] lp = new String[] {Manifest.permission.ACCESS_FINE_LOCATION};
-                                ActivityCompat.requestPermissions(MainActivity.this,lp,2);
-                            }
-                        }
+                    case R.id.audioSwitch:
+                       permisoAudio();
+                        break;
+                    case R.id.ubicacionSwitch:
+                        permisoUbicacion();
                         break;
                 }
             }
         };
 
-        btncamera.setOnClickListener(click);
-        btngps.setOnClickListener(click);
-
-
+        camaraSwitch.setOnClickListener(click);
+        audioSwitch.setOnClickListener(click);
+        ubicacionSwitch.setOnClickListener(click);
     }
+
+
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
@@ -77,21 +63,123 @@ Button btncamera, btngps;
         switch (requestCode)
         {
             case 1:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
-
+                if (grantResults.length > 0  && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permiso de cámara aceptado :D", Toast.LENGTH_SHORT).show();
+                    camaraSwitch.setChecked(true);
+                }
+                else
+                    {
+                    Toast.makeText(this, "Permiso de cámara rechazado D:", Toast.LENGTH_SHORT).show();
+                    camaraSwitch.setChecked(false);
                 }
                 break;
-
             case 2:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                if (grantResults.length > 0  && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permiso de internet aceptado :D", Toast.LENGTH_SHORT).show();
+                    audioSwitch.setChecked(true);
+                }
+                else
                 {
-
+                    Toast.makeText(this, "Permiso de internet rechazado :c", Toast.LENGTH_SHORT).show();
+                    audioSwitch.setChecked(false);
+                }
+                break;
+            case 3:
+                if (grantResults.length > 0  && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permiso de ubicación aceptado :D", Toast.LENGTH_SHORT).show();
+                    ubicacionSwitch.setChecked(true);
+                }
+                else
+                {
+                    Toast.makeText(this, "Permiso de ubicación rechazado :c", Toast.LENGTH_SHORT).show();
+                    ubicacionSwitch.setChecked(false);
                 }
                 break;
         }
     }
 
+    public void permisoCamara()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            {
 
+                if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.CAMERA)) {
+                    String[] lp = new String[]{Manifest.permission.CAMERA};
+                    ActivityCompat.requestPermissions(MainActivity.this, lp, 1);
+                    camaraSwitch.setChecked(false);
+                }
+                else
+                {
+
+                    String[] lp = new String[] {Manifest.permission.CAMERA};
+                    ActivityCompat.requestPermissions(MainActivity.this,lp,1);
+                    camaraSwitch.setChecked(false);
+                }
+            }
+            else
+            {
+                camaraSwitch.setChecked(true);
+            }
+        }
+
+    }
+
+    public void permisoAudio()
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+            {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.RECORD_AUDIO)) {
+                    String[] lp = new String[]{Manifest.permission.RECORD_AUDIO};
+                    ActivityCompat.requestPermissions(MainActivity.this, lp, 2);
+                    audioSwitch.setChecked(false);
+                }
+                else
+                {
+
+                    String[] lp = new String[] {Manifest.permission.RECORD_AUDIO};
+                    ActivityCompat.requestPermissions(MainActivity.this,lp,2);
+                   audioSwitch.setChecked(false);
+                }
+            }
+           else
+            {
+                audioSwitch.setChecked(true);
+            }
+        }
+
+    }
+
+    public void permisoUbicacion()
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.ACCESS_FINE_LOCATION)) { Toast.makeText(MainActivity.this, "Aceptalo", Toast.LENGTH_SHORT).show();
+                    String[] lp = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+                    ActivityCompat.requestPermissions(MainActivity.this, lp, 3);
+                    ubicacionSwitch.setChecked(false);
+                }
+                else
+                {
+
+                    String[] lp = new String[] {Manifest.permission.ACCESS_FINE_LOCATION};
+                    ActivityCompat.requestPermissions(MainActivity.this,lp,3);
+                    ubicacionSwitch.setChecked(false);
+                }
+            }
+            else
+            {
+                ubicacionSwitch.setChecked(true);
+            }
+        }
+
+    }
 
 }
